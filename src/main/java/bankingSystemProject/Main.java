6436527609dc4e51710f9bb0e4dd7f94b4fa38a0
AleksandraOrzechowska <aorzechowska_999@wp.pl>
2018-05-19 22:00:17 +0200

    Adding the method for searching the account

--- src/main/java/bankingSystemProject/Main.java
+++ src/main/java/bankingSystemProject/Main.java
@@ -1,16 +1,21 @@
 package bankingSystemProject;
 
 
+import javax.swing.*;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-	
+
+    private static Scanner x;
+
 	public static void main(String[] args) {
 
-	    List<Account> accounts = new LinkedList<Account>();
+        List<Account> accounts = new LinkedList<Account>();
 
-		String file = "C:\\Users\\Aleksandra\\Desktop\\bankSystemData.csv";
+        String file = "C:\\Users\\Aleksandra\\Desktop\\bankSystemData.csv";
 
 		/*
 	CheckingAccount checking1  = new CheckingAccount("Ewa Kowalska", "11112222333", 3000);
@@ -25,8 +30,8 @@ public class Main {
 
 	*/
 
-		List<String[]> newAccountOwners = bankingSystemProject.CSV.read(file);
-		for(String[] accountOwner : newAccountOwners){
+        List<String[]> newAccountOwners = bankingSystemProject.CSV.read(file);
+        for (String[] accountOwner : newAccountOwners) {
 
 
             String name = accountOwner[0];
@@ -34,28 +39,70 @@ public class Main {
             String accountType = accountOwner[2];
             double initialDeposit = Double.parseDouble(accountOwner[3]);
 
-            if (accountType.equals("Checking")){
+            if (accountType.equals("Checking")) {
 
                 accounts.add(new CheckingAccount(name, pesel, initialDeposit));
 
-            }
-            else if (accountType.equals("Deposit")){
+            } else if (accountType.equals("Deposit")) {
 
                 accounts.add(new DepositAccount(name, pesel, initialDeposit));
-            }
-
-            else {
+            } else {
                 System.out.println("Error reading account type");
             }
         }
 
 
-        for(Account acc : accounts){
+        /*for(Account acc : accounts){
 		    System.out.println("\n************************");
 		    acc.showInformation();
         }
+
+        */
         //accounts.get(1).showInformation();
 
-	}
-	
+
+
+        String accountType;
+        //Calling the method for searching of the account, getting the data
+        System.out.println("Insert name and account type");
+
+
+    }
+        public static void searchAccount(String name, String accountType, String file) {
+	    boolean found = false;
+            try {
+                String customer = "";
+                String ID = "";
+                String type = "";
+                String balanceOfTheAccount = "";
+
+
+                x = new Scanner(new File(file));
+                x.useDelimiter("[,\n]");
+
+                while (x.hasNext() && !found)
+
+                {
+                    customer = x.next();
+                    if (customer.equals(name) && accountType.equals(type)){
+                        found = true;
+                    }
+                }
+                if (found ){
+                    JOptionPane.showMessageDialog(null,"Balance equals"+ balanceOfTheAccount);
+                }
+                else  {
+                    JOptionPane.showMessageDialog(null,"Account not found");
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null,"Error");
+            }
+
+
+        }
+
+
+
+
+
 }
